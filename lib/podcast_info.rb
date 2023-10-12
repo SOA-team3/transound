e004c22c7eded30cc08727469bd95d5619f7882b
Brian <brian2000890714@gmail.com>
2023-10-12 15:49:28 +0800

    podcast_info modify

--- lib/podcast_info.rb
+++ lib/podcast_info.rb
@@ -1,31 +1,67 @@
 # frozen_string_literal: true
-
 require 'http'
 require 'yaml'
+require 'json'
 
-config = YAML.safe_load_file('config/secrets.yml')
-puts config
+config = YAML.safe_load_file('../config/secrets.yml')
+CLIENT_ID = config["spotify_Client_ID"]
+CLIENT_SECRET = config["spotify_Client_secret"]
 
 
-def spt_api_path(path)
-  "https://api.github.com/#{path}"
+def spotify_api_path(id, category, market)
+  "https://api.spotify.com/v1/#{category}/#{id}?market=#{market}"
 end
 
-def call_spt_url(config, url)
-  HTTP.headers('Accept' => 'application/vnd.github.v3+json',
-               'Authorization' => "token #{config['GITHUB_TOKEN']}").get(url)
+def call_spotify_url(config, url)
+  # Bearer token
+  token = get_temp_TOKEN
+  # Create headers with the Bearer token
+  headers = {
+    "Authorization" => "Bearer #{token}"
+  }
+  # Send a GET request
+  response = HTTP.headers(headers).get(url)
 end
 
-spt_response = {}
-spt_results = {}
+def get_temp_TOKEN
+  # Define the URL for the token endpoint
+  token_url = "https://accounts.spotify.com/api/token"
 
-# ## HAPPY project request
-# project_url = gh_api_path('repos/soumyaray/YPBT-app')
-# gh_response[project_url] = call_gh_url(config, project_url)
-# project = gh_response[project_url].parse
 
-# gh_results['size'] = project['size']
-# # should be 551
+  # Define the parameters for the POST request
+  params = {
+    grant_type: "client_credentials"
+  }
+
+  # Set the basic authentication header with your client ID and secret
+  auth_header = {
+    Authorization: "Basic " + Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")
+  }
+
+  # Make the POST request to the token endpoint
+  response = HTTP.headers(auth_header)
+                .post(token_url, form: params)
+
+  # Print the response
+  # puts response.body
+  json_body = JSON.parse(response.body)
+  access_token = json_body["access_token"]
+end
+# puts get_temp_TOKEN
+
+podcast_response = {}
+podcast_results = {}
+
+## HAPPY project request
+project_url = spotify_api_path( id = "5Vv32KtHB3peVZ8TeacUty",
+                                category = "shows",
+                                market = "TW",)
+podcast_response[project_url] = call_spotify_url(config, project_url)
+project = podcast_response[project_url].parse
+
+podcast_results['description'] = project['description']
+# should be description of some podcast
+puts podcast_results
 
 # gh_results['owner'] = project['owner']
 # # should have info about Soumya

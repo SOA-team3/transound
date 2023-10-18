b96d12d3ce7ccbbdb93f16dcb92c15428ae1a7aa
tsaiadrian <tsaiadrian414@gmail.com>
2023-10-18 17:08:01 +0800

    branch test-tools done

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'yaml'
 # testing
 gem 'minitest', '~> 5'
 gem 'minitest-rg', '~> 5'
+gem 'minitest-unit'
 gem 'simplecov', '~> 0'
 gem 'vcr', '~> 6'
 gem 'webmock', '~> 3'
--- lib/podcast_api.rb
+++ lib/podcast_api.rb
@@ -73,68 +73,85 @@ module TranSound
 
   # Apply token or Get token
   class Token
-    SECRET_PATH = 'config/secrets.yml'
-    CONFIG = YAML.safe_load_file(SECRET_PATH)
-    CLIENT_ID = CONFIG['spotify_Client_ID']
-    CLIENT_SECRET = CONFIG['spotify_Client_secret']
-    GETTOKEN_TIME = CONFIG['spotify_gettoken_time']
-    TEMP_TOKEN = CONFIG['spotify_temp_token']
+    def initialize
+      @secret_path = 'config/secrets.yml'
+      @config = YAML.safe_load_file(@secret_path)
+    end
 
     def get
-      # puts "Time_difference_of_getting_token: #{time_difference_of_get_token}"
-      if time_difference_of_get_token >= 55
-        access_token = apply_for_new_temp_token
+      # puts "Time_difference_of_getting_token: #{TokenTime.time_difference_of_get_token}"
+      if TokenTime.new(@config).time_difference_of_get_token >= 55
+        access_token = ApplyForNewTempToken.new(@config['spotify_Client_ID'],
+                                                @config['spotify_Client_secret']).apply_for_new_temp_token
         # save the temp token
-        save_temp_token(access_token)
+        SaveTempToken.new(@secret_path, @config).save_temp_token(access_token)
         return access_token
       end
 
-      TEMP_TOKEN
+      @config['spotify_temp_token']
+    end
+  end
+
+  # apply for new temp token
+  class ApplyForNewTempToken
+    def initialize(client_id, client_secret)
+      @client_id = client_id
+      @client_secret = client_secret
     end
 
+    # apply for new temp token
     def apply_for_new_temp_token
-      # Define the URL for the token endpoint
-      token_url = 'https://accounts.spotify.com/api/token'
       # Define the parameters for the POST request
       params = {
         grant_type: 'client_credentials'
       }
       # Set the basic authentication header with your client ID and secret
       auth_header = {
-        Authorization: "Basic #{Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")}"
+        Authorization: "Basic #{Base64.strict_encode64("#{@client_id}:#{@client_secret}")}"
       }
       # Make the POST request to the token endpoint
-      response = HTTP.headers(auth_header).post(token_url, form: params)
+      response = HTTP.headers(auth_header).post('https://accounts.spotify.com/api/token', form: params)
       # puts response.body
       json_body = JSON.parse(response.body)
       json_body['access_token']
     end
+  end
+
+  # this can save temp token to secrets.yml
+  class SaveTempToken
+    def initialize(secret_path, config)
+      @secret_path = secret_path
+      @config = config
+      @taipei_timezone = TZInfo::Timezone.get('Asia/Taipei')
+    end
 
     def save_temp_token(access_token)
       # Modify the value of spotify_gettoken_time and spotify_temp_token
-      config = YAML.safe_load_file(SECRET_PATH)
-      config['spotify_gettoken_time'] = current_datetime.strftime('%Y%m%d%H%M%S')
-      config['spotify_temp_token'] = access_token
+      @config['spotify_gettoken_time'] = @taipei_timezone.now.strftime('%Y%m%d%H%M%S')
+      @config['spotify_temp_token'] = access_token
       # Save the updated YAML back to the file
-      File.write(SECRET_PATH, config.to_yaml)
+      File.write(@secret_path, @config.to_yaml)
+    end
+  end
+
+  # record token time in taipei time
+  class TokenTime
+    def initialize(config)
+      @config = config
+      @gettoken_time = @config['spotify_gettoken_time']
+      @taipei_timezone = TZInfo::Timezone.get('Asia/Taipei')
     end
 
-    def current_datetime
-      # Create a TZInfo::Timezone object for Taipei, Taiwan
-      taipei_timezone = TZInfo::Timezone.get('Asia/Taipei')
-      # Get the current time in Taipei
-      taipei_timezone.now
-    end
-
+    # get token time
     def gettoken_time
-      date_time_str = GETTOKEN_TIME
+      date_time_str = @gettoken_time
       # Convert the string to a Time object
       Time.strptime(date_time_str, '%Y%m%d%H%M%S')
     end
 
     def time_difference_of_get_token
       # Calculate the time difference in minutes
-      ((current_datetime - gettoken_time) / 60).to_i
+      ((@taipei_timezone.now - gettoken_time) / 60).to_i
     end
   end
 end

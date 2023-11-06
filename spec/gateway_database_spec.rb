102c4b4bf90e123a8fc25e0a5fcdc8f899d64da1
tsaiadrian <tsaiadrian414@gmail.com>
2023-11-06 14:27:14 +0800

    start branch

--- spec/gateway_database_spec.rb
+++ spec/gateway_database_spec.rb
@@ -26,11 +26,31 @@ describe 'Integration Tests of Spotify API and Database' do
       rebuilt = TranSound::Repository::For.entity(episode).create(episode)
 
       _(rebuilt.origin_id).must_equal(episode.origin_id)
-      _(rebuilt.name).must_equal(episode.name)
       _(rebuilt.description).must_equal(episode.description)
       _(rebuilt.images).must_equal(episode.images)
       _(rebuilt.language).must_equal(episode.language)
+      _(rebuilt.name).must_equal(episode.name)
       _(rebuilt.release_date).must_equal(episode.release_date)
+      _(rebuilt.type).must_equal(episode.type)
+    end
+  end
+
+  describe 'Retrieve and store show' do
+    before do
+      DatabaseHelper.wipe_database
+    end
+
+    it 'HAPPY: should be able to save *show* from Spotify API to database' do
+      show = TranSound::Podcast::ShowMapper.new(TEMP_TOKEN).find(SHOW_TYPE, SHOW_ID, MARKET)
+
+      rebuilt = TranSound::Repository::For.entity(show).create(show)
+
+      _(rebuilt.origin_id).must_equal(show.origin_id)
+      _(rebuilt.description).must_equal(show.description)
+      _(rebuilt.images).must_equal(show.images)
+      _(rebuilt.name).must_equal(show.name)
+      _(rebuilt.publisher).must_equal(show.publisher)
+      _(rebuilt.type).must_equal(show.type)
     end
   end
 end

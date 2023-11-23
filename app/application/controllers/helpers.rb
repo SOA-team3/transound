2e6720ccfbebe7ebff91aa15d5b3374f24564ae3
tsaiadrian <tsaiadrian414@gmail.com>
2023-11-23 19:32:11 +0800

    merge

--- app/application/controllers/helpers.rb
+++ app/application/controllers/helpers.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-module CodePraise
-    module RouteHelpers
-      # Application value for the path of a requested project
-      class PodcastInfoRequestPath
-        def initialize(type, id, request)
-          @type = type
-          @id = id
-          @request = request
-          @path = request.remaining_path
-        end
-  
-        attr_reader :owner_name, :project_name
-  
-        def folder_name
-          @folder_name ||= @path.empty? ? '' : @path[1..]
-        end
-      end
-    end
-  end
-=======
 module TranSound
   module RouteHelpers
     # Application value for the path of a requested project
@@ -34,4 +13,3 @@ module TranSound
     end
   end
 end
->>>>>>> 3435dcb13ec8337b581bfdf1fafc14ecce5d1824
